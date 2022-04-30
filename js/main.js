5596b2c99608077c82a17cba42ec7a46edd11ad0
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-04-30 11:25:43 -0600

    Created repo. Added files for week 2

---
+++ js/main.js
@@ -0,0 +1,27 @@
+const links = [
+    {
+        label: "Week1",
+        url: "week1/index.html",
+    },
+    {
+        label: "Week2",
+        url: "week2/index.html"
+    }
+];
+
+const ol = document.getElementsByTagName('ol')[0];
+
+function createLinks(){
+    for (let i = 0; i < links.length; i++){
+        var li = document.createElement("li");
+        var a = document.createElement("a")
+
+        a.appendChild(document.createTextNode(links[i].label))
+        a.setAttribute("href", links[i].url)
+
+        li.appendChild(a);
+        ol.appendChild(li);
+    }
+}
+
+createLinks()
