f4ca9ebb384daf43581085032ad36efc4f372539
brysonprince <59940182+brysonprince@users.noreply.github.com>
2022-04-30 11:58:14 -0600

    cleanup

--- week2/practice/chessboard.js
+++ week2/practice/chessboard.js
@@ -3,7 +3,7 @@ let board = '';
 
 for(let y = 1; y <= size; y++){
     for(let x = 1; x <= size; x++){
-      if((y % 2 == 0 && x % 2) == 0 || (y % 2 != 0 && x % 2 != 0)){
+      if((y % 2 == 0 && x % 2 == 0) || (y % 2 != 0 && x % 2 != 0)){
         board += '#';
       }
       else{
